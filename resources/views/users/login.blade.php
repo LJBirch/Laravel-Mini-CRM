f76f926f08fa863dfb826eb8e11f68d6174485c8
Lewis Birch <l.birch@lancaster.ac.uk>
2022-09-07 13:11:18 +0100

    Created UserController and added login route.

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 
     <form action="">
+        <label for="email">Email:</label>
         <input name="email" type="text">
+
+        <label for="password">Password:</label>
         <input name="password" type="password">
         <button type="submit">Login</button>
     </form>
