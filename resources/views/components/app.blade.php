77053e018f914a51fef36d8d2129694f5f7e0c33
Lewis Birch <l.birch@lancaster.ac.uk>
2022-09-08 21:27:06 +0100

    Checkpoint commit.

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -21,16 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarColor02">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
+                        <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" aria-current="page" href="/">Home</a>
                     </li>
                 </ul>
 
