1afe5eb7e45ba1bb35e5001774a6ad22611456e6
Lewis Birch <l.birch@lancaster.ac.uk>
2022-09-08 22:52:41 +0100

    Added bootstrap styles to employee edit route.

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -1,32 +1,60 @@
 @extends('components.app')
 
 @section('content')
-    {{--  #TODO add validation error messages  --}}
-    <form method="POST" action="/employees/{{$employee->id}}">
-        @csrf
-        @method('PUT')
-        <label for="first_name">First Name:</label>
-        <input name="first_name" type="text" value="{{$employee->first_name}}">
-
-        <label for="last_name">Last Name:</label>
-        <input name="last_name" type="text" value="{{$employee->last_name}}">
-
-        <label for="email">Email:</label>
-        <input name="email" type="email" value="{{$employee->email}}">
-
-        <label for="phone_number">Phone Number:</label>
-        <input name="phone_number" type="string" value="{{$employee->phone_number}}">
-
-        <label for="company_id">Company</label>
-        <select name="company_id" id="company_id">
-            @foreach($companies as $company)
-                <option value="{{$company->id}}" {{$employee->company_id == $company->id ? 'selected' : ''}}>
-                    {{$company->name}}
-                </option>
-            @endforeach
-        </select>
-
-        <button type="submit">Submit</button>
-    </form>
-
+<x-section>
+    <div class="row justify-content-center align-items-center">
+        <div class="col-md-5">
+            <div class="card">
+                <div class="card-body">
+                    <form method="POST" action="/employees/{{$employee->id}}" class="form">
+                        @csrf
+                        @method('PUT')
+                        <h3 class="text-center mb-3">Create Employee</h3>
+                        <div class="mb-3">
+                            <label for="first_name" class="form-label">First Name</label>
+                            <input name="first_name" type="text" class="form-control" value="{{$employee->first_name}}">
+                            @error('first_name')
+                            <div class="form-text text-danger">{{$message}}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="last_name" class="form-label">Last Name</label>
+                            <input name="last_name" type="text" class="form-control" value="{{$employee->last_name}}">
+                            @error('last_name')
+                            <div class="form-text text-danger">{{$message}}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input name="email" type="email" class="form-control" value="{{$employee->email}}">
+                            @error('email')
+                            <div class="form-text text-danger">{{$message}}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="phone_number" class="form-label">Phone Number</label>
+                            <input name="phone_number" type="string" class="form-control" value="{{$employee->phone_number}}">
+                            @error('phone_number')
+                            <div class="form-text text-danger">{{$message}}</div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="company_id" class="form-label">Company</label>
+                            <select class="form-select" name="company_id" id="company_id">
+                                @foreach($companies as $company)
+                                <option value="{{$company->id}}" {{$employee->company_id == $company->id ? 'selected' : ''}}>
+                                    {{$company->name}}
+                                </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-section>
 @endsection
