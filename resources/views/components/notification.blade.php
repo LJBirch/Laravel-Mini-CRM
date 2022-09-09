a3b9e06a506ba8b0a977d5ebccbf9c587e4e5081
Lewis Birch <l.birch@lancaster.ac.uk>
2022-09-09 13:01:39 +0100

    Added alpinejs to enable notification timeout after three seconds.

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,11 +1,12 @@
 @if(session()->has('message'))
-<div class="toast-container position-absolute p-3 end-0 bottom-0">
-    <div class="toast align-items-center show bg-primary text-white" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="d-flex">
-            <div class="toast-body">
-                {{session('message')}}
+<div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show">
+    <div class="toast-container position-absolute p-3 end-0 bottom-0">
+        <div class="toast align-items-center show bg-primary text-white" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="d-flex">
+                <div class="toast-body">
+                    {{session('message')}}
+                </div>
             </div>
-            <button type="button" class="btn-close btn-close-white me-2 m-auto"></button>
         </div>
     </div>
 </div>
